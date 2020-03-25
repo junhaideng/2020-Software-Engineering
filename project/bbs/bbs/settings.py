5ef8a4bcfca2c3ae596f8c0f8cf02fd007696f6f
Cyberist-Edgar <201648748@qq.com>
2020-03-25 13:17:18 +0800

    config 配置静态文件

--- project/bbs/bbs/settings.py
+++ project/bbs/bbs/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'community',
+    'course',
+    'search',
+    'home',
+    'post',
+    'user',
+    'api'
 ]
 
 MIDDLEWARE = [
@@ -119,3 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = "/static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
