529c8c1371406eb1bbdf9692812881126a0a70ce
Cyberist-Edgar <201648748@qq.com>
2020-03-25 13:38:27 +0800

    实现搜索前端，并使用ajax获取后台数据，但后端尚未完成

--- project/bbs/search/views.py
+++ project/bbs/search/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
 
-# Create your views here.
+
+def search(request):
+    """搜索相关"""
+    if request.method == 'POST':  # 处理ajax对应的事件，可以实时的获取信息
+        return JsonResponse({"data": ["success", "course"]})
+    if request.method == "GET":  #
+        keyword = request.GET.get("keyword")
+        if keyword:  # 如果含有keyword处理 点击search按钮之后的事件
+            data = None
+            return render(request, 'search/search.html', context={"keyword":keyword, "data":data})
+        return render(request, 'search/index.html')  # 否则只是简单的访问该网页
