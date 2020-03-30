71b304a3b55567a6c36118907eccd2330ab6b6fc
Cyberist-Edgar <201648748@qq.com>
2020-03-30 15:43:44 +0800

    add 添加反馈相关的数据库模型

--- project/bbs/feedback/models.py
+++ project/bbs/feedback/models.py
@@ -1,3 +1,20 @@
+"""
+author: Edgar
+反馈界面的主要数据库模型
+"""
 from django.db import models
 
-# Create your models here.
+
+class Feedback(models.Model):
+    user_id = models.IntegerField()  # 用户id
+    username = models.CharField(max_length=30)  # 用户名
+    subject = models.CharField(max_length=100)  # 主题
+    content = models.TextField()  # 反馈内容
+    feedback_type = models.BooleanField(default=True)  # True -> 建议  False -> 举报
+
+    class Meta:
+        ordering = ("username", )
+
+    def __str__(self):
+        return self.subject
+
