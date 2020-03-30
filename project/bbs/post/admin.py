7b0c4d274bd098c2c2ae55ab86f0b80c32ffc974
Cyberist-Edgar <201648748@qq.com>
2020-03-30 15:46:27 +0800

    feat 实现对帖子相关数据库的管理注册

--- project/bbs/post/admin.py
+++ project/bbs/post/admin.py
@@ -1,3 +1,18 @@
-from django.contrib import admin
+"""
+author: Edgar
+对应用post下的数据库在admin界面进行管理注册
+"""
 
-# Register your models here.
+from django.contrib import admin
+from .models import *
+
+
+class AdminPost(admin.ModelAdmin):
+    """对数据库Post在admin进行管理"""
+    list_display = ("topic", "counter", "content", "created_time")  # 显示的属性
+    list_per_page = 30  # 每页的数据数
+    ordering = ("-created_time", "topic")  # 排列方式
+    search_fields = ("topic",)  # 搜索区可以搜到哪些区域
+
+
+admin.site.register(Post, AdminPost)  # 进行注册
