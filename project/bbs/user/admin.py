e28823cbe77e838384cfe50647a7813a4f70b50f
Cyberist-Edgar <201648748@qq.com>
2020-03-30 16:22:27 +0800

    feat 实现对用户相关的数据库的管理

--- project/bbs/user/admin.py
+++ project/bbs/user/admin.py
@@ -1,3 +1,18 @@
+"""
+author: Edgar
+对user下的数据库模型进行管理
+"""
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class AdminUser(admin.ModelAdmin):
+    list_display = ("user", "sex", "academy", "grade")  # 显示信息
+    ordering = ("user",)  # 排序信息
+    list_per_page = 30  # 每页显示的数据量
+    search_fields = ("user", "academy")  # 搜索区域
+
+
+admin.site.register(User, AdminUser)
+
+
