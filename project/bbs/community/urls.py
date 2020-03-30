a08f66a9285f3373bacad9eafc74e7a1e5386c42
Cyberist-Edgar <201648748@qq.com>
2020-03-30 15:37:27 +0800

    add 添加帖子详细信息的路由

--- project/bbs/community/urls.py
+++ project/bbs/community/urls.py
@@ -1,7 +1,12 @@
+"""
+author: Edgar
+对社区路由下的子路由进行配置
+"""
 from django.urls import path
 from .views import *
 
 app_name = "community"
 urlpatterns = [
-    path("", index, name='index')
+    path("", index, name='index'),  # 社区的主页
+    path("post/<int:id>", detail, name='detail'),  # 每个帖子的详情页
 ]
