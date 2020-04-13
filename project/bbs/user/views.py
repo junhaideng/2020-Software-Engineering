cc9a42829aba949b5ad664d1b6d2b625c105569e
Cyberist-Edgar <201648748@qq.com>
2020-04-13 14:11:30 +0800

    feat 实现个人用户文件的查询下载和删除

--- project/bbs/user/views.py
+++ project/bbs/user/views.py
@@ -3,16 +3,18 @@ author: Edgar
 实现用户的相关操作
 """
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
-from django.http import HttpResponseRedirect
-from django.http import HttpResponse
 from django.urls import reverse
 from django.views.decorators.http import require_http_methods
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import authenticate
-from .models import User
+from .models import User, ExpData
 from django.contrib.auth.models import User as AuthUser
+import datetime
+import time
+
 
 @require_http_methods(["GET", "POST"])
 def forget(request):
@@ -23,8 +25,8 @@ def forget(request):
         if flag:
             u = User.objects.get(user__username=username)
             Question = u.question
-            request.session['msg']=username
-            return render(request, 'user/forgetdetail.html', context={"message":None,"question":Question})
+            request.session['msg'] = username
+            return render(request, 'user/forgetdetail.html', context={"message": None, "question": Question})
         else:
             message = "此用户不存在"
             request.session['user_forget_message'] = message
@@ -33,21 +35,22 @@ def forget(request):
         message = "请输入用户名"
         if request.session.get("user_forget_message"):
             message = request.session.pop("user_forget_message")
-        return render(request, 'user/forget.html', context={"message":message})
+        return render(request, 'user/forget.html', context={"message": message})
+
 
 @require_http_methods(["GET", "POST"])
 def forgetdetail(request):
     """验证密保 author 祁山青
     ToDo: 验证三个密保，两个正确即可重置
     """
-    username=request.session.get('msg')
-    u=User.objects.get(user__username=username)
-    RightAnswer=u.answer
-    Question=u.question
+    username = request.session.get('msg')
+    u = User.objects.get(user__username=username)
+    RightAnswer = u.answer
+    Question = u.question
     if request.method == "POST":
         answer = request.POST.get("answer")
-        if answer==RightAnswer:
-            #重置密码
+        if answer == RightAnswer:
+            # 重置密码
             user1 = AuthUser.objects.get(username=username)
             user1.set_password('123456')
             user1.save()
@@ -61,7 +64,8 @@ def forgetdetail(request):
         message = None
         if request.session.get('user_answer_message'):
             message = request.session.pop('user_answer_message')
-        return render(request, 'user/forgetdetail.html', context={"message":message,"question":Question})
+        return render(request, 'user/forgetdetail.html', context={"message": message, "question": Question})
+
 
 @require_http_methods(["GET", "POST"])
 def login(request):
@@ -74,7 +78,7 @@ def login(request):
             user = authenticate(username=username, password=password)  # 进行账号密码验证
             if user:  # 登录成功
                 auth_login(request, user)
-                request.session['username']=username
+                request.session['username'] = username
                 if request.POST.get("next"):
                     return redirect(request.POST.get("next"))
                 return redirect("/")
@@ -88,7 +92,7 @@ def login(request):
         message = None
         if request.session.get("user_login_message"):
             message = request.session.pop("user_login_message")
-        return render(request, 'user/login.html', context={"message":message})
+        return render(request, 'user/login.html', context={"message": message})
 
 
 @require_http_methods(["GET", "POST"])
@@ -120,6 +124,7 @@ def signup(request):
     else:
         return render(request, 'user/signup.html')
 
+
 @login_required()
 @require_http_methods(["GET"])
 def logout(request):
@@ -141,8 +146,29 @@ def history(request):
     return render(request, 'user/history.html')
 
 
+@login_required()
+@require_http_methods(["GET", "POST"])
 def file(request):
-    return render(request, 'user/file.html')
+    """用户上传的文件(实验数据)
+    @author： Edgar
+    """
+    if request.method == "POST":  # POST 代表删除文件
+        file_id = request.POST.get("id")  # 获取文件的id
+        if file_id:
+            ExpData.objects.get(id=file_id).delete()  # 删除文件
+            return JsonResponse({"status": "success"})  # 删除成功
+        return JsonResponse({"status": "failed"})   # 无此id
+    else:
+        user_id = request.user.id
+        files = ExpData.objects.filter(user_id=user_id)  # 获取该用户的所有文件
+        data = []
+        if files:
+            for file in files:
+                data.append(
+                    {"name": file.name, "path": file.path, "downloads": file.download_times, "exp_type": file.exp_type,
+                     "date": file.date, "id": file.id})
+        return render(request, 'user/file.html', context={"files": data})
+
 
 @login_required()
 @require_http_methods(["GET", "POST"])
@@ -151,14 +177,14 @@ def resetpwd(request):
     author 祁山青
     """
     if request.method == "POST":
-        username=request.session['username']
+        username = request.session['username']
         u = AuthUser.objects.get(username=username)
-        oldpwd=request.POST.get("oldpwd")
+        oldpwd = request.POST.get("oldpwd")
         newpwd = request.POST.get("newpwd")
-        confirm=request.POST.get("confirm")
+        confirm = request.POST.get("confirm")
         res = authenticate(username=username, password=oldpwd)  # 进行账号密码验证
         if res:
-            if confirm!=newpwd:
+            if confirm != newpwd:
                 message = "两次密码不正确"
                 # 重置密码
             else:
