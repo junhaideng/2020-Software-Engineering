6ca48d05bb1f11fc232547cda406bac2c65b615f
Cyberist-Edgar <201648748@qq.com>
2020-04-14 12:05:39 +0800

    fix 将主页和搜索合并

--- project/bbs/home/views.py
+++ project/bbs/home/views.py
@@ -4,5 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     """主页面"""
-    profile = None
-    return render(request, 'home/index.html', locals())
+    return render(request, 'home/index.html')
--- project/bbs/search/views.py
+++ project/bbs/search/views.py
@@ -3,8 +3,10 @@ author: Edgar
 对搜索界面的显示进行处理
 TODO: 用户输入的数据后回车，或者点击按钮的界面进行处理 此时url中有参数 keyword， 收集课程的信息
 """
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
+from django.urls import reverse
+
 from course.models import Course
 
 
@@ -23,4 +25,4 @@ def search(request):
         if keyword:  # 如果含有keyword处理 点击search按钮之后的事件
             data = None
             return render(request, 'search/search.html', context={"keyword": keyword, "data": data})
-        return render(request, 'search/index.html')  # 否则只是简单的访问该网页
+        return redirect(reverse("home:index"))  # 否则只是简单的访问该网页
