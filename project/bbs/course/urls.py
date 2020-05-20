17def8335b5e81b9c8131f239ce56092e5499c56
Cyberist-Edgar <201648748@qq.com>
2020-05-20 15:22:09 +0800

    实现从数据库中获取所有的课程api

--- project/bbs/course/urls.py
+++ project/bbs/course/urls.py
@@ -5,6 +5,7 @@ app_name = "course"
 urlpatterns = [
     path("", index, name='index'),
     path("upload/", upload, name='upload'),
-    path("details/<str:type>/<str:school>/<int:page_num>",details,name='details'),
-    path("coursedes/<int:pk>", coursedes, name='coursedes')
+    path("details/<str:type>/<str:school>/<int:page_num>", details, name='details'),
+    path("coursedes/<int:pk>", coursedes, name='coursedes'),
+    path("get_all_course/", get_all_course, name="get_all"),  # 获取所有的课程
 ]
--- project/bbs/course/views.py
+++ project/bbs/course/views.py
@@ -9,7 +9,7 @@ from django.http import JsonResponse
 from django.core.paginator import Paginator
 from django.http import HttpResponse
 from django.views.decorators.http import require_http_methods
-from .models import Course,TeacherOfCourse,CourseDes,Major,CourseCom
+from .models import Course, TeacherOfCourse, CourseDes, Major, CourseCom
 from user.models import ExpData
 from user.models import User
 import time
@@ -24,36 +24,37 @@ TYPES = (  # 课程的类型
     ("Ot", '其他')  # 其他
 )
 
-SCHOOLS=(   #   学院名称
-    ("1","电子信息与电气工程学院"),
-    ("2","机械与动力工程学院"),
-    ("3","船舶海洋与建筑工程学院"),
-    ("4","生物医学工程学院"),
-    ("5","航空航天学院"),
-    ("6","数学科学院"),
-    ("7","物理与天文学院"),
-    ("8","化学化工学院"),
-    ("9","致远学院"),
-    ("10","医学院"),
-    ("11","安泰经济与管理学院"),
-    ("12","人文学院"),
-    ("13","材料科学与工程学院"),
-    ("14","海洋学院"),
-    ("15","药学院"),
-    ("16","生命科学技术学院"),
-    ("17","农业与生物学院"),
-    ("18","凯原法学院"),
-    ("19","外国语学院"),
-    ("20","体育系"),
-    ("21","马克思主义学院"),
-    ("22","国际公共与事务学院"),
-    ("23","上海高级金融学院")
+SCHOOLS = (  # 学院名称
+    ("1", "电子信息与电气工程学院"),
+    ("2", "机械与动力工程学院"),
+    ("3", "船舶海洋与建筑工程学院"),
+    ("4", "生物医学工程学院"),
+    ("5", "航空航天学院"),
+    ("6", "数学科学院"),
+    ("7", "物理与天文学院"),
+    ("8", "化学化工学院"),
+    ("9", "致远学院"),
+    ("10", "医学院"),
+    ("11", "安泰经济与管理学院"),
+    ("12", "人文学院"),
+    ("13", "材料科学与工程学院"),
+    ("14", "海洋学院"),
+    ("15", "药学院"),
+    ("16", "生命科学技术学院"),
+    ("17", "农业与生物学院"),
+    ("18", "凯原法学院"),
+    ("19", "外国语学院"),
+    ("20", "体育系"),
+    ("21", "马克思主义学院"),
+    ("22", "国际公共与事务学院"),
+    ("23", "上海高级金融学院")
 )
 
-@require_http_methods(["GET","POST"])
+
+@require_http_methods(["GET", "POST"])
 def index(request):
     """课程界面的首页"""
-    #啥都不干
+    # 啥都不干
     return render(request, 'course/index.html')
 
 
@@ -73,93 +74,104 @@ def upload(request):
         ExpData.objects.create(user_id=request.user.id,
                                exp_type=exp_type,
                                download_times=0,
-                               path="/media/experiment_data/"+alias,
+                               path="/media/experiment_data/" + alias,
                                name=os.path.splitext(file.name)[0]).save()
         return JsonResponse({"status": "success"})
 
 
-@require_http_methods(["GET","POST"])
-def details(request,type,school,page_num):
+@require_http_methods(["GET", "POST"])
+def details(request, type, school, page_num):
     """课程界面的首页"""
-    class Newcourse(): #建立一个新的类
+
+    class Newcourse():  # 建立一个新的类
         def __init__(self):
-            self.pk= None #课程主键
+            self.pk = None  # 课程主键
             self.name = '无'  # 课程的名字
-            self.school ='无'  # 专业名
-            self.teacher= [] #老师 可能有多个
-    page_now=page_num#当前页码
-    Type=type
-    School= school
-    if Type=="0":#我将没有选择的时候设置为了0
-        courselist=Course.objects.filter(school=School)
-    elif School=="0":
+            self.school = '无'  # 专业名
+            self.teacher = []  # 老师 可能有多个
+
+    page_now = page_num  # 当前页码
+    Type = type
+    School = school
+    if Type == "0":  # 我将没有选择的时候设置为了0
+        courselist = Course.objects.filter(school=School)
+    elif School == "0":
         courselist = Course.objects.filter(type=Type)
     else:
-        courselist = Course.objects.filter(school=School,type=Type)
-    List=[]
-    for course in courselist:#将教师和课程 组合在一个列表中
-        a=Newcourse()
-        a.name=course.name
-        a.pk=course.pk
-        a.school=SCHOOLS[int(course.school)-1][1]
-        info=TeacherOfCourse.objects.filter(course_id=course.pk)
-        if info.count()==0:
-            a.teacher=['None']
+        courselist = Course.objects.filter(school=School, type=Type)
+    List = []
+    for course in courselist:  # 将教师和课程 组合在一个列表中
+        a = Newcourse()
+        a.name = course.name
+        a.pk = course.pk
+        a.school = SCHOOLS[int(course.school) - 1][1]
+        info = TeacherOfCourse.objects.filter(course_id=course.pk)
+        if info.count() == 0:
+            a.teacher = ['None']
         else:
             for b in TeacherOfCourse.objects.filter(course_id=course.pk):
                 a.teacher.append(b.name)
         List.append(a)
     p = Paginator(List, 2)
-    total_num=p.num_pages #页数
-    pagesNum=[]#页码列表
-    i=1
+    total_num = p.num_pages  # 页数
+    pagesNum = []  # 页码列表
+    i = 1
     while i <= total_num:
         pagesNum.append(i)
         i = i + 1
-    page=p.page(page_num) #对应分页
-    pre_page=page_now-1
-    if pre_page==0:
-        pre_page=1
-    next_page=page_now+1
-    if next_page==total_num+1:
-        next_page=page_now
-    return render(request, 'course/details.html', {"list": page,"pagesNum":pagesNum,"total_num":total_num,
-                                               "now_page":page_now,"pre_page":pre_page,"next_page":next_page})
+    page = p.page(page_num)  # 对应分页
+    pre_page = page_now - 1
+    if pre_page == 0:
+        pre_page = 1
+    next_page = page_now + 1
+    if next_page == total_num + 1:
+        next_page = page_now
+    return render(request, 'course/details.html', {"list": page, "pagesNum": pagesNum, "total_num": total_num,
+                                                   "now_page": page_now, "pre_page": pre_page, "next_page": next_page})
 
-@require_http_methods(["GET","POST"])
-def coursedes(request,pk):
-    course=Course.objects.get(pk=pk)
-    teachers=TeacherOfCourse.objects.filter(course_id=course.pk)
-    flag=1
-    if teachers.count()==0:
-        flag=0
+
+@require_http_methods(["GET", "POST"])
+def coursedes(request, pk):
+    course = Course.objects.get(pk=pk)
+    teachers = TeacherOfCourse.objects.filter(course_id=course.pk)
+    flag = 1
+    if teachers.count() == 0:
+        flag = 0
     if (CourseDes.objects.filter(course_id=course.pk)).count() == 0:
-        des='暂无描述'
+        des = '暂无描述'
     else:
         des = CourseDes.objects.get(course_id=course.pk)
     for i in TYPES:
-        if i[0]==course.type:
-            type=i[1]
-    school=SCHOOLS[int(course.school)-1][1]
-    #返回用户评论
-    comments=CourseCom.objects.filter(courseid=course.pk)
+        if i[0] == course.type:
+            type = i[1]
+    school = SCHOOLS[int(course.school) - 1][1]
+    # 返回用户评论
+    comments = CourseCom.objects.filter(courseid=course.pk)
     uerloginflag = 0
     commentflag = 0
-    if 'username' in request.session :
-        uerloginflag=1
-    if ('username' in request.session)and request.method == "POST":
-        comment=request.POST.get("comment")
-        username=request.session['username']
+    if 'username' in request.session:
+        uerloginflag = 1
+    if ('username' in request.session) and request.method == "POST":
+        comment = request.POST.get("comment")
+        username = request.session['username']
         u = User.objects.get(user__username=username)
-        newcomment=CourseCom()
-        newcomment.courseid=course
-        newcomment.com=comment
-        newcomment.user_name=username
+        newcomment = CourseCom()
+        newcomment.courseid = course
+        newcomment.com = comment
+        newcomment.user_name = username
         newcomment.save()
-        commentflag=1
+        commentflag = 1
     print(uerloginflag)
-    return render(request,'course/coursedes.html',{"course":course,"teacherList":teachers,"des":des,"school":school,
-                                                   "type":type,"flag":flag,"userflag":uerloginflag,
-                                                   "commentflag":commentflag,"comments":comments})
+    return render(request, 'course/coursedes.html',
+                  {"course": course, "teacherList": teachers, "des": des, "school": school,
+                   "type": type, "flag": flag, "userflag": uerloginflag,
+                   "commentflag": commentflag, "comments": comments})
 
 
+@require_http_methods(["GET"])
+def get_all_course(request):
+    """ 从数据库中获取所有的课程"""
+    courses = set()
+    for i in Course.objects.values("name"):
+        courses.add(i.get('name'))
+    return JsonResponse({"data": list(courses)})
