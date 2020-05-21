ac0bbbfd49c1659ac4d96867b077df3a15d28eba
SanqingQi <1294516355@qq.com>
2020-05-21 23:08:23 +0800

    修改了课程数据库的院系问题

--- project/bbs/course/views.py
+++ project/bbs/course/views.py
@@ -25,29 +25,29 @@ TYPES = (  # 课程的类型
 )
 
 SCHOOLS=(   #   学院名称
-    ("1","电子信息与电气工程学院"),
-    ("2","机械与动力工程学院"),
-    ("3","船舶海洋与建筑工程学院"),
-    ("4","生物医学工程学院"),
-    ("5","航空航天学院"),
-    ("6","数学科学院"),
-    ("7","物理与天文学院"),
-    ("8","化学化工学院"),
-    ("9","致远学院"),
-    ("10","医学院"),
-    ("11","安泰经济与管理学院"),
-    ("12","人文学院"),
-    ("13","材料科学与工程学院"),
-    ("14","海洋学院"),
-    ("15","药学院"),
-    ("16","生命科学技术学院"),
-    ("17","农业与生物学院"),
-    ("18","凯原法学院"),
-    ("19","外国语学院"),
-    ("20","体育系"),
-    ("21","马克思主义学院"),
-    ("22","国际公共与事务学院"),
-    ("23","上海高级金融学院")
+    ("电子信息与电气工程学院","1"),
+    ("机械与动力工程学院","2"),
+    ("船舶海洋与建筑工程学院","3"),
+    ("生物医学工程学院","4"),
+    ("航空航天学院","5"),
+    ("数学科学院","6"),
+    ("物理与天文学院","7"),
+    ("化学化工学院","8"),
+    ("致远学院","9"),
+    ("医学院","10"),
+    ("安泰经济与管理学院","11"),
+    ("人文学院","12"),
+    ("材料科学与工程学院","13"),
+    ("海洋学院","14"),
+    ("药学院","15"),
+    ("生命科学技术学院","16"),
+    ("农业与生物学院","17"),
+    ("凯原法学院","18"),
+    ("外国语学院","19"),
+    ("体育系","20"),
+    ("马克思主义学院","21"),
+    ("国际公共与事务学院","22"),
+    ("上海高级金融学院","23")
 )
 
 @require_http_methods(["GET","POST"])
@@ -85,23 +85,33 @@ def details(request,type,school,page_num):
         def __init__(self):
             self.pk= None #课程主键
             self.name = '无'  # 课程的名字
-            self.school ='无'  # 专业名
+            self.school ='无'  # 开设学院名
             self.teacher= [] #老师 可能有多个
     page_now=page_num#当前页码
     Type=type
     School= school
+    #school 数字变为对应字符
     if Type=="0":#我将没有选择的时候设置为了0
+        for m in SCHOOLS:
+            if m[1] == School:
+                School = m[0]
         courselist=Course.objects.filter(school=School)
     elif School=="0":
+        for m in SCHOOLS:
+            if m[1] == School:
+                School = m[0]
         courselist = Course.objects.filter(type=Type)
     else:
+        for m in SCHOOLS:
+            if m[1] == School:
+                School = m[0]
         courselist = Course.objects.filter(school=School,type=Type)
     List=[]
     for course in courselist:#将教师和课程 组合在一个列表中
         a=Newcourse()
         a.name=course.name
         a.pk=course.pk
-        a.school=SCHOOLS[int(course.school)-1][1]
+        a.school=School
         info=TeacherOfCourse.objects.filter(course_id=course.pk)
         if info.count()==0:
             a.teacher=['None']
@@ -148,7 +158,7 @@ def coursedes(request,pk):
     for i in TYPES:
         if i[0]==course.type:
             type=i[1]
-    school=SCHOOLS[int(course.school)-1][1]
+    school=course.school
     #返回用户评论
     comments=CourseCom.objects.filter(courseid=course.pk)
     new_comments=[]       #返回给前端的列表
