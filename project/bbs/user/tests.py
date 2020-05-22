0169b3358d121960867ed45e7790be2af91e955d
Cyberist-Edgar <201648748@qq.com>
2020-05-22 15:07:55 +0800

    完成部分测试

--- project/bbs/user/tests.py
+++ project/bbs/user/tests.py
@@ -1,22 +1,23 @@
-from django.test import TestCase
 from django.urls import reverse
-from selenium.webdriver.support import wait
-
 from .models import User, AuthUser  # AuthUser 是django提供的
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 
 
 class Test(StaticLiveServerTestCase):
+    """
+    author: Edgar
+    """
     def setUp(self) -> None:
         auth = AuthUser(username="test", email="test@qq.com")
         auth.set_password("123456")  # 设置密码，不能够直接设置
         auth.save()
-        user = User(user=auth, sex=True)
+        user = User(user=auth, sex=True, question="tests", answer="answer")  # 用户写入数据库，并设置密保
         user.save()
         self.browser = webdriver.Firefox()
 
-    def test_login(self):
+    def test_login_and_logout(self):
+        """用户的登录和登出"""
         url = self.live_server_url + reverse("user:login")
         self.browser.get(url)
         username = self.browser.find_element_by_id("username")
@@ -28,8 +29,12 @@ class Test(StaticLiveServerTestCase):
         self.browser.implicitly_wait(1)  # 等待1s加载
         result = self.browser.find_element_by_xpath("/html/body/div/nav/div/div/div/div")  # 登录之后会出现个人中心
         self.assertIsNotNone(result.get_attribute("innerHTML"))  # 因为该元素需要触发然后才能获取到text内容，这里直接判断存在值
+        self.browser.find_element_by_xpath('/html/body/div/nav/div/div/div/a').click()  # 点击头像，出现选项
+        self.browser.find_element_by_xpath('/html/body/div/nav/div/div/div/div/a[3]').click()  # 登出
+        self.assertEqual(self.browser.find_element_by_xpath('/html/body/div/nav/div/a[1]').text, "登录")   # 登出之后，会出现登出二字
 
     def test_signup(self):
+        """用户的注册"""
         url = self.live_server_url + reverse("user:signup")
         self.browser.get(url)
         self.browser.find_element_by_xpath('//*[@id="username"]').send_keys("hello")  # 输入用户名
@@ -39,6 +44,20 @@ class Test(StaticLiveServerTestCase):
         self.browser.find_element_by_xpath('//*[@id="btn"]').click()  # 点击注册
         result = self.browser.find_element_by_xpath("/html/body/div/nav/div/div/div/div")  # 注册之后会自动登录，登录之后会出现个人中心
         self.assertIsNotNone(result.get_attribute("innerHTML"))
+
+    def test_forget(self):
+        """密码忘记根据密保找回"""
+        url = self.live_server_url + reverse("user:forget")
+        self.browser.get(url)
+        self.browser.find_element_by_xpath('//*[@id="username"]').send_keys("test")  # 输入用户名
+        self.browser.find_element_by_xpath('//*[@id="btn"]').click()  # 点击提交到另一个界面
+        self.browser.find_element_by_xpath('//*[@id="answer"]').send_keys("answer")  # 回答问题
+        self.browser.find_element_by_xpath('//*[@id="btn"]').click()  # 提交
+        message = self.browser.find_element_by_xpath('/html/body/div/div/div')  # 重置成功之后会有提示信息
+        self.assertEqual(message.text, "您的密码已经重置为:123456 请返回主页重新登陆")
+
+    def test_reset_password(self):
+        pass
 
     def tearDown(self) -> None:
         self.browser.close()
