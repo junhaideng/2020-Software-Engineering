4fde297bc7387264470d5e254e5857903112983d
Cyberist-Edgar <201648748@qq.com>
2020-05-22 14:43:14 +0800

    主页的测试

--- project/bbs/home/tests.py
+++ project/bbs/home/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from selenium import webdriver
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.urls import reverse
+from selenium.common.exceptions import NoSuchElementException
 
-# Create your tests here.
+
+class Test(StaticLiveServerTestCase):
+    """
+    author: Edgar
+    """
+    def setUp(self) -> None:
+        self.browser = webdriver.Firefox()
+
+    def test_home(self):
+        url = self.live_server_url + reverse("home:index")  # 主页
+        self.browser.get(url)
+        try:
+            element = self.browser.find_element_by_xpath('//*[@id="search"]')  # 搜索框
+        except NoSuchElementException:
+            element = None
+        self.assertIsNotNone(element)  # 正常情况下是可以找到搜索框的
+
+    def tearDown(self) -> None:
+        self.browser.close()
