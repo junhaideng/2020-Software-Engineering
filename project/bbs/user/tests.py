44b923707aee0b115edf823f7ce2dd58bb2987eb
sznswjr <2444936458@qq.com>
2020-05-25 14:09:00 +0800

    用户资料及修改的测试

--- project/bbs/user/tests.py
+++ project/bbs/user/tests.py
@@ -8,6 +8,7 @@ class Test(StaticLiveServerTestCase):
     """
     author: Edgar
     """
+
     def setUp(self) -> None:
         auth = AuthUser(username="test", email="test@qq.com")
         auth.set_password("123456")  # 设置密码，不能够直接设置
@@ -31,7 +32,7 @@ class Test(StaticLiveServerTestCase):
         self.assertIsNotNone(result.get_attribute("innerHTML"))  # 因为该元素需要触发然后才能获取到text内容，这里直接判断存在值
         self.browser.find_element_by_xpath('/html/body/div/nav/div/div/div/a').click()  # 点击头像，出现选项
         self.browser.find_element_by_xpath('/html/body/div/nav/div/div/div/div/a[3]').click()  # 登出
-        self.assertEqual(self.browser.find_element_by_xpath('/html/body/div/nav/div/a[1]').text, "登录")   # 登出之后，会出现登出二字
+        self.assertEqual(self.browser.find_element_by_xpath('/html/body/div/nav/div/a[1]').text, "登录")  # 登出之后，会出现登出二字
 
     def test_signup(self):
         """用户的注册"""
@@ -62,3 +63,64 @@ class Test(StaticLiveServerTestCase):
     def tearDown(self) -> None:
         self.browser.close()
 
+    def test_profile(self):
+        """
+        用户资料显示
+        author: 吴嘉锐
+        """
+        url = self.live_server_url + reverse("user:signup")  # 先进行注册
+        self.browser.get(url)
+        self.browser.find_element_by_xpath('//*[@id="username"]').send_keys("hello")  # 输入用户名
+        self.browser.find_element_by_xpath('//*[@id="email"]').send_keys("123@qq.com")  # 输入邮箱
+        self.browser.find_element_by_xpath('//*[@id="password"]').send_keys("123456")  # 输入密码
+        self.browser.find_element_by_xpath('//*[@id="confirm"]').send_keys("123456")  # 确认密码
+        self.browser.find_element_by_xpath('//*[@id="btn"]').click()  # 点击注册
+
+        url = self.live_server_url + reverse("user:profile")  # 再测试用户信息
+        self.browser.get(url)
+        # 获取显示的用户各项资料
+        name = self.browser.find_element_by_xpath('/html/body/div/div/div/div[2]/div/div[2]/text()').text
+        sex = self.browser.find_element_by_xpath('/html/body/div/div/div/div[2]/div/div[3]/text()').text
+        academy = self.browser.find_element_by_xpath('/html/body/div/div/div/div[2]/div/div[4]/text()').text
+        grade = self.browser.find_element_by_xpath('/html/body/div/div/div/div[2]/div/div[5]/text()').text
+        self.assertEqual(name, "hello")
+        self.assertEqual(sex, "男")
+        self.assertEqual(academy, "您还没有设置学院哦！")
+        self.assertEqual(grade, "未知")
+
+    def test_modify(self):
+        """
+            用户资料修改
+            author: 吴嘉锐
+        """
+        url = self.live_server_url + reverse("user:signup")  # 先进行注册
+        self.browser.get(url)
+        self.browser.find_element_by_xpath('//*[@id="username"]').send_keys("hello")  # 输入用户名
+        self.browser.find_element_by_xpath('//*[@id="email"]').send_keys("123@qq.com")  # 输入邮箱
+        self.browser.find_element_by_xpath('//*[@id="password"]').send_keys("123456")  # 输入密码
+        self.browser.find_element_by_xpath('//*[@id="confirm"]').send_keys("123456")  # 确认密码
+        self.browser.find_element_by_xpath('//*[@id="btn"]').click()  # 点击注册
+
+        url = self.live_server_url + reverse("user:modify")  # 修改个人资料
+        self.browser.get(url)
+        name = self.browser.find_element_by_id("nick")  # 修改用户名“modify_test”
+        name.send_keys("modify_test")
+        self.browser.find_element_by_xpath("/html/body/div/div/div/div[2]/div/form/div[2]/div").click()  # 修改性别为女
+        self.browser.find_element_by_xpath("/html/body/div/div/div/div[2]/div/form/div[2]/div/select/option[3]").click()
+        self.browser.find_element_by_xpath("/html/body/div/div/div/div[2]/div/form/div[3]/div").click()  # 修改学院船建
+        self.browser.find_element_by_xpath("/html/body/div/div/div/div[2]/div/form/div[3]/div/select/option[2]").click()
+        self.browser.find_element_by_xpath("/html/body/div/div/div/div[2]/div/form/div[4]/div").click()  # 修改年级大二
+        self.browser.find_element_by_xpath("/html/body/div/div/div/div[2]/div/form/div[4]/div/select/option[3]").click()
+        self.browser.find_element_by_xpath("/html/body/div/div/div/div[2]/div/form/div[6]/button").click()  # 确认修改
+
+        url = self.live_server_url + reverse("user:profile")  # 是否修改成功
+        self.browser.get(url)
+        # 获取显示的用户各项资料
+        name = self.browser.find_element_by_xpath('/html/body/div/div/div/div[2]/div/div[2]/text()').text
+        sex = self.browser.find_element_by_xpath('/html/body/div/div/div/div[2]/div/div[3]/text()').text
+        academy = self.browser.find_element_by_xpath('/html/body/div/div/div/div[2]/div/div[4]/text()').text
+        grade = self.browser.find_element_by_xpath('/html/body/div/div/div/div[2]/div/div[5]/text()').text
+        self.assertEqual(name, "modify_test")
+        self.assertEqual(sex, "女")
+        self.assertEqual(academy, "船舶海洋与建筑工程学院")
+        self.assertEqual(grade, "大二")
