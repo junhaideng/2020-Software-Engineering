53d357b09d7cbe69a407f301f20e85577a9cbd28
Cyberist-Edgar <201648748@qq.com>
2020-05-26 16:18:01 +0800

    优化

--- project/bbs/course/tests.py
+++ project/bbs/course/tests.py
@@ -21,6 +21,7 @@ class Test(StaticLiveServerTestCase):
         auth.set_password("123456")
         auth.save()
         User.objects.create(user=auth)
+        # 如果不关联对应的用户，则会报错！ 这是由于views.py 查询comment的问题
         CourseCom.objects.create(courseid=course, com="Test", user_name="test")
         self.client = Client()
         self.browser = webdriver.Firefox()
@@ -46,13 +47,12 @@ class Test(StaticLiveServerTestCase):
         element = self.browser.find_element_by_xpath('/html/body/div/table/tbody/tr[1]/td[2]')  # 找到课程的名称
         self.assertEqual(element.text, "数据结构")
 
-    # def tearDown(self) -> None:
-    #     self.browser.close()
+    def tearDown(self) -> None:
+        self.browser.close()
 
     def test_comment(self):
         """测试课程评论系统"""
         url = self.live_server_url + reverse("course:coursedes", args=[3])  # 这里1表示的是id为1的课程，也就是 `数据结构`
-        print(url)
         self.browser.get(url)
         comment = self.browser.find_element_by_xpath('/html/body/div/div[1]/div[2]/div[2]')  # 找到对应评论
         self.assertEqual(comment.text, "Test")
